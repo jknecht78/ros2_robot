78d6de9af2bc0901c9afe19ffd169841c5717405
jknecht78 <joachim.hums@gmail.com>
2023-12-04 09:08:19 +0100

    cleanup:     @echo "Cleaning up..."     @rm -rf $(BUILD_DIR)     @rm -rf $(BIN_DIR)     @rm -rf $(OBJ_DIR)     @rm -rf $(DEP_DIR)     @rm -rf $(TEST_DIR)     @rm -rf $(TEST_OBJ_DIR)     @rm -rf $(TEST_DEP_DIR)     @rm -rf $(TEST_BIN_DIR)     @rm -rf $(TEST_REPORT_DIR)     @rm -rf $(TEST_REPORT_DIR

--- motor_control/motor_controll.ino
+++ motor_control/motor_controll.ino
@@ -1,56 +1,118 @@
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+struct Motor {
+    int channel1;
+    int channel2;
+    int speed;
+};
+  
+Motor motor_front_left;
+Motor motor_front_right;
+Motor motor_rear_left;
+Motor motor_rear_right;
 
-Adafruit_PWMServoDriver myServos = Adafruit_PWMServoDriver();
+void setup() {  
 
-#define SERVOMIN  150
-#define SERVOMAX  600
+  motor_front_left.channel1 = 8;
+  motor_front_left.channel2 = 9;
+  motor_front_left.speed = 10;
 
-int current_x = (SERVOMIN + SERVOMAX) / 2;
-int current_y = (SERVOMIN + SERVOMAX) / 2;
+  motor_front_right.channel1 = 13;
+  motor_front_right.channel2 = 12;
+  motor_front_right.speed = 11;
 
-uint8_t servonum = 0;
-uint8_t numberOfServos = 16;
+  motor_rear_left.channel1 = 5;
+  motor_rear_left.channel2 = 7;
+  motor_rear_left.speed = 6;
 
-void setup() {
-  myServos.begin();
-  myServos.setPWMFreq(60);
+  motor_rear_right.channel1 = 2;
+  motor_rear_right.channel2 = 4;
+  motor_rear_right.speed = 3;
+
+
+  pinMode(motor_front_left.channel1, OUTPUT);
+  pinMode(motor_front_left.channel2, OUTPUT);
+  pinMode(motor_front_left.speed, OUTPUT);
+
+  pinMode(motor_front_right.channel1, OUTPUT);
+  pinMode(motor_front_right.channel2, OUTPUT);
+  pinMode(motor_front_right.speed, OUTPUT); 
+
+  pinMode(motor_rear_left.channel1, OUTPUT);
+  pinMode(motor_rear_left.channel2, OUTPUT);
+  pinMode(motor_rear_left.speed, OUTPUT);
+
+  pinMode(motor_rear_right.channel1, OUTPUT);
+  pinMode(motor_rear_right.channel2, OUTPUT);
+  pinMode(motor_rear_right.speed, OUTPUT); 
+
+  Serial.begin(96000);
+}
+
+void adjust_speed(int speed) {
+  analogWrite(motor_front_left.speed, speed);
+  analogWrite(motor_front_right.speed, speed);
+  analogWrite(motor_rear_left.speed, speed);
+  analogWrite(motor_rear_right.speed, speed); 
+}
+
+void stop() {
+  adjust_speed(0); 
+  digitalWrite(motor_front_left.channel1, LOW);
+  digitalWrite(motor_front_left.channel2, LOW);
+
+  digitalWrite(motor_front_right.channel1, LOW);
+  digitalWrite(motor_front_right.channel2, LOW);
+
+  digitalWrite(motor_rear_left.channel1, LOW);
+  digitalWrite(motor_rear_left.channel2, LOW);
+
+  digitalWrite(motor_rear_right.channel1, LOW);
+  digitalWrite(motor_rear_right.channel2, LOW);  
   delay(10);
-  Serial.begin(9600);
+}
+
+void forward(int speed) {
+  stop();
+
+  digitalWrite(motor_front_left.channel1, HIGH);
+  digitalWrite(motor_front_left.channel2, LOW);
+
+  digitalWrite(motor_front_right.channel1, HIGH);
+  digitalWrite(motor_front_right.channel2, LOW);
+
+  digitalWrite(motor_rear_left.channel1, HIGH);
+  digitalWrite(motor_rear_left.channel2, LOW);
+
+  digitalWrite(motor_rear_right.channel1, HIGH);
+  digitalWrite(motor_rear_right.channel2, LOW);
+
+  delay(10);
+  adjust_speed(100); 
+}
+
+void backward(int speed) {
+  stop();
+
+  digitalWrite(motor_front_left.channel1, HIGH);
+  digitalWrite(motor_front_left.channel2, LOW);
+
+  digitalWrite(motor_front_right.channel1, HIGH);
+  digitalWrite(motor_front_right.channel2, LOW);
+
+  digitalWrite(motor_rear_left.channel1, HIGH);
+  digitalWrite(motor_rear_left.channel2, LOW);
+
+  digitalWrite(motor_rear_right.channel1, HIGH);
+  digitalWrite(motor_rear_right.channel2, LOW);
+
+  delay(10);
+  adjust_speed(100); 
 }
 
 void loop() {
-  if (Serial.available()) {
-    String data = Serial.readStringUntil('\n'); 
-    int commaIndex = data.indexOf(',');
-    int x = data.substring(0, commaIndex).toInt(); 
-    int y = data.substring(commaIndex + 1).toInt(); 
-    yourFunction(x, y);
-  }
-}
+  int speed = 100;
 
-void yourFunction(int x, int y) {
-  if ( (x < 0) and (current_x - 1 > SERVOMIN) ) {
-    current_x--;
-  }
-
-  if ( (y < 0) and (current_y - 1 > SERVOMIN) ) {
-    current_y--;
-  }
-
-  if ( (x > 0) and (current_x + 1 < SERVOMAX) ) {
-    current_x++;
-  }
-
-  if ( (y > 0) and (current_y + 1 < SERVOMAX) ) {
-    current_y++;
-  }
-
-  if ( x != 0) {
-    myServos.setPWM(0, 0, current_x);
-  }
-  
-  if ( y != 0) {
-    myServos.setPWM(1, 0, current_y);
-  }
+  forward(speed);
+  delay(1000);
+  backward(speed);
+  delay(1000);
 }
