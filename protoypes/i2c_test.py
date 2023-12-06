3ac260c2ea4751a4d5c9ac9fa65da4f0ba661d49
jknecht78 <jknecht78@users.noreply.github.com>
2023-12-06 19:52:59 +0100

    Update i2c_test.py

--- protoypes/i2c_test.py
+++ protoypes/i2c_test.py
@@ -1,1 +1,22 @@
+from smbus2 import SMBus, i2c_msg
 
+# I2C bus
+bus = SMBus(1)
+
+# I2C slave address
+address = 0x10
+
+# Create a write message to send a request
+write = i2c_msg.write(address, [])
+
+# Create a read message to receive the data
+read = i2c_msg.read(address, 1024)
+
+# Send the request and receive the data
+bus.i2c_rdwr(write, read)
+
+# Convert the received data to a string
+data = ''.join(chr(b) for b in read)
+
+# Print the received data
+print(data)
